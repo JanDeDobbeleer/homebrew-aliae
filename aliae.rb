a0db67350f2f65e0a3720dd5455c6a36473a8455
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2023-07-05 06:44:20 +0000

    feat: formula v0.1.0

--- aliae.rb
+++ aliae.rb
@@ -1,11 +1,11 @@
-class OhMyPosh < Formula
-  desc "Prompt theme engine for any shell"
+class Aliae < Formula
+  desc "Cross shell and platform alias management"
   homepage "https://aliae.dev"
-  url "https://github.com/JanDeDobbeleer/aliae/archive/v17.5.3.tar.gz"
+  url "https://github.com/JanDeDobbeleer/aliae/archive/v0.1.0.tar.gz"
   head "https://github.com/JanDeDobbeleer/aliae.git", branch: "main"
-  sha256 "de2855ef79f9232d724929cfd3ed9d70f97d613c3f05b9325c5ca5a7a3b48a19"
+  sha256 "e0a43a5cc87a9fb2cd87fca3ff1d5614a75716bf69a4eb2be50adfe45e5d9632"
   license "MIT"
-  version "17.5.3"
+  version "0.1.0"
 
   depends_on "go@1.20" => :build
 
@@ -20,7 +20,7 @@ class OhMyPosh < Formula
   def caveats
     <<~EOS
       Thanks for installing aliae.
-      Have a look at https://aliae.dev/docs/ for detailed instructions for your shell.
+      Have a look at https://www.aliae.dev/docs/configuration/shell for detailed instructions for your shell.
 
       If you're enjoying aliae, feel free to donate or become a sponsor.
       https://github.com/sponsors/JanDeDobbeleer
