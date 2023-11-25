4db21b76f64ef01bc4d822f75fcbc845306a787b
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2023-11-25 10:52:52 +0000

    feat: formula v0.17.0

--- Casks/aliae.rb
+++ Casks/aliae.rb
@@ -2,10 +2,10 @@ cask "aliae" do
     desc "Cross shell and platform alias management"
     homepage "https://aliae.dev"
     arch arm: "arm64", intel: "amd64"
-    version "0.16.0"
+    version "0.17.0"
     url "https://github.com/JanDeDobbeleer/aliae/releases/download/v#{version}/aliae-darwin-#{arch}"
-    sha256 arm:   "ba57ca788a4059c4fe8867f22db176d1bcc4105714145754b83359e3ff9ba1fd",
-           intel: "20ad98dd22eec38736e299f640d8f85adc8fc10c57d48e10222c9ab6a86e330f"
+    sha256 arm:   "68f1898dfd5c872b300378737fa5bd69b4fed55fd2cf0f65a68cf02aeeaeca54",
+           intel: "4b04f89c8411c55b827a9779e68ab916c6dabde5091de4d65e60d9d0094ad48a"
     name "aliae"
     binary "aliae-darwin-#{arch}", target: "aliae"
     auto_updates true
