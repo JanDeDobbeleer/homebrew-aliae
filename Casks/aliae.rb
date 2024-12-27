7d6b79bf1f3b802f7fb7b70ac4c5a2469a72d066
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2024-12-27 12:26:37 +0000

    feat: formula v0.24.0

--- Casks/aliae.rb
+++ Casks/aliae.rb
@@ -2,10 +2,10 @@ cask "aliae" do
     desc "Cross shell and platform alias management"
     homepage "https://aliae.dev"
     arch arm: "arm64", intel: "amd64"
-    version "0.23.0"
+    version "0.24.0"
     url "https://github.com/JanDeDobbeleer/aliae/releases/download/v#{version}/aliae-darwin-#{arch}"
-    sha256 arm:   "f0b441abf864c14c4de948ba42cba2e2a57a01368856dc8e3301d807214d1e47",
-           intel: "ddb4c069f326d7369c67b4ad4e6b759f0ebd7b8b46fa7959f3407205c02766dd"
+    sha256 arm:   "447cd60922f5f45538d0644a6e04c6ff92093003fafc7aead78819013431ff7a",
+           intel: "5d7ca434a0fe0d1ff9e949cc330779fd510d6b1d28b254cb57932da110d89fab"
     name "aliae"
     binary "aliae-darwin-#{arch}", target: "aliae"
     auto_updates true
--- aliae.rb
+++ aliae.rb
@@ -1,18 +1,18 @@
 class Aliae < Formula
   desc "Cross shell and platform alias management"
   homepage "https://aliae.dev"
-  url "https://github.com/JanDeDobbeleer/aliae/archive/v0.23.0.tar.gz"
+  url "https://github.com/JanDeDobbeleer/aliae/archive/v0.24.0.tar.gz"
   head "https://github.com/JanDeDobbeleer/aliae.git", branch: "main"
-  sha256 "080d243fbd333f2684989ccdd71bd3069ff36db6ac968acc656d4e1bbd924b34"
+  sha256 "8036ac29dcd7aa4c259bd69b0554d3d3de1b08db25943f0999b698c5ad3dc19f"
   license "MIT"
-  version "0.23.0"
+  version "0.24.0"
 
   depends_on "go@1.22" => :build
 
   def install
     Dir.chdir("src") do
       ENV["GOPROXY"] = ENV.has_key?("HOMEBREW_GOPROXY") ? ENV["HOMEBREW_GOPROXY"] : ""
-      system("go build -o=aliae -ldflags=\"-s -w -X \'main.Version=0.23.0\'\"")
+      system("go build -o=aliae -ldflags=\"-s -w -X \'main.Version=0.24.0\'\"")
       bin.install "aliae"
     end
   end
